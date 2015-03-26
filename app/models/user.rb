4f6a9d776006875c6b49f8b6a95e9b3c433c697b
Andrew Chung <andrew.chung159@gmail.com>
2015-03-26 15:43:09 -0400

    Add validates constraints to User name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	#{ presence: true } is a one element options hash. 
+	#curly braces may be omitted when a hash is the final argument in a method.
+
+	#same as validates(:name, { presence: true })
+	validates :name, presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 255 },
+			  		 format: { with: VALID_EMAIL_REGEX },
+			  		 #rails infers uniqueness: true below
+			  		 uniqueness: { case_sensitive: false }
 end
