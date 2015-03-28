fb94b2f8b34a83c2d2a27582b5816e4e52546863
Andrew Chung <andrew.chung159@gmail.com>
2015-03-27 23:13:10 -0400

    Add User#show and show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  	#uncomment below to debug in rails server (uses byebug gem)
+  	#debugger
+  end
+
   def new
   end
 end
