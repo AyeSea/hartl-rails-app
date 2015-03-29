b4226a3673297672de10371da10e80719b54c021
Andrew Chung <andrew.chung159@gmail.com>
2015-03-29 15:04:02 -0400

    Add ability to log user in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+
+	#Logs in the given user.
+	#Temporary cookies created using the session method
+	#below are automatically encrypted.
+	def log_in(user)
+		session[:user_id] = user.id
+	end
 end
