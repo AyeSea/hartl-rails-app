0bb59721e9d51b3d8fac14161841f0f0d4b94cde
Andrew Chung <andrew.chung159@gmail.com>
2015-03-29 17:46:24 -0400

    Add remember feature using permanent cookies/remember tokens

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,11 +7,27 @@ module SessionsHelper
 		session[:user_id] = user.id
 	end
 
-	#Returns the current logged-in user (if any), otherwise
-	#defines the current user and returns it.
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
+	# Remember a user in a persistent session.
+	def remember(user)
+		# Creates token and saves remember_digest to db.
+		user.remember
+		# Creates permanent cookies for encrypted user_id and remember token.
+		cookies.permanent.signed[:user_id] = user.id
+		cookies.permanent[:remember_token] = user.remember_token
 	end
+
+	#Returns the user corresponding to the remember token cookie.
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
 
 	#Returns true if the user is logged in, false otherwise.
 	def logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	#{ presence: true } is a one element options hash. 
 	#curly braces may be omitted when a hash is the final argument in a method.
@@ -19,5 +20,22 @@ class User < ActiveRecord::Base
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def User.new_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+
+  def remember
+  	self.remember_token = User.new_token
+  	update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
