753f1f5b4b85c92c7ca412ed050f053e5c94e554
Andrew Chung <andrew.chung159@gmail.com>
2015-03-30 11:36:32 -0400

    Add index view for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,16 @@
 class UsersController < ApplicationController
   #a filter method that runs before any given action(s)
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
 
   def show
   	@user = User.find(params[:id])
   	#uncomment below to debug in rails server (uses byebug gem)
   	#debugger
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
